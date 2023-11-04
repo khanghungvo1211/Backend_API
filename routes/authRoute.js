25d93e9d42891637dc329fe75155b51e82082c83
khanghungvo1211 <hungkhang02@gmail.com>
2023-11-04 19:04:15 +0700

    sos

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,6 +2,32 @@ const express = require("express");
 const { createUser, loginUserController, getaUser, getallUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken } = require("../controller/userController");
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
 const router = express.Router();
+/**
+ * @swagger
+ * /api/user/register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Register a new user with the provided information.
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully.
+ *       400:
+ *         description: Bad request or user already exists.
+ */
 router.post("/register", createUser);
 router.post("/forgot-password-token", forgotPasswordToken);
 router.put('/password', authMiddleware, updatePassword);
