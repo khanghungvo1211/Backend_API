0fe7d6f50042fbd3360a883367fb707f41ebe341
khanghungvo1211 <hungkhang02@gmail.com>
2023-11-04 18:40:12 +0700

    4_11

---
+++ routes/authRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { createUser, loginUserController, getaUser, getallUser, deleteaUser, updateaUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken } = require("../controller/userController");
+const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
+const router = express.Router();
+router.post("/register", createUser);
+router.post("/forgot-password-token", forgotPasswordToken);
+router.put('/password', authMiddleware, updatePassword);
+router.post("/login", loginUserController);
+router.get("/all-users", getallUser);
+router.get("/refresh", handleRefreshToken);
+router.get('/logout', logout);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
+router.delete("/:id", deleteaUser);
+router.put("/edit-user", authMiddleware, updateaUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
+module.exports = router;
